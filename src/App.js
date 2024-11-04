10c2a13f82df9b53829ae2d81f75b6bd4a9f26d4
Ishajoshi02 <ishajoshi361@georgebrown.ca>
2024-11-03 23:31:13 -0500

    lab07

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import logo from './logo.svg'; // Assuming you have the React logo in `src` folder or replace with a URL
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} className="App-logo" alt="React logo" />
+        <h1>Welcome to Fullstack Development - I</h1>
+        <h2>React JS Programming Week09 Lab Exercise</h2>
+        <p>101343698</p>
+        <p>Isha Joshi</p>
+        <p>George Brown College, Toronto</p>
       </header>
     </div>
   );
